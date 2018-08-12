4558628bf86b70e6d00b18939f25379ac342d692
nijhum_jawad2000 <njawad@techlaunch.io>
2018-08-11 20:02:09 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,24 @@
+// console.log("Works")
+
+var x=9, y=10;
+function add(num1, num2){
+  console.log(num1 +num2)
+  //return num1 + num2
+}
+add(x,y)
+var sum = add(20, 35)
+ console.log(sum + sum)
+
+function randomValue(){
+  return Math.floor(Math.random()*10 +1)
+}
+ console.log(randomValue())
+
+function sumUp3(x,y,z){
+  console.log(x)
+  console.log(y)
+  console.log(z)
+  console.log(x + y+ z)
+}
+
+sumUp3(randomValue(), randomValue(), randomValue())
